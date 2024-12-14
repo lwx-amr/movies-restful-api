7f6f7bc6aa5c397ad576c43c13f926b0eb9ae8e8
lwx-amr <amr.ister20@gmail.com>
2024-12-14 20:41:02 +0200

    [DP-10] Introduce Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config({ path: process.cwd() + '/.env' });
 
@@ -10,6 +11,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder().setTitle('Movies Restful API').setDescription('API Documentation').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   const port = configService.get<number>('app.port');
   await app.listen(port);
--- src/modules/database-sync/database-sync.controller.ts
+++ src/modules/database-sync/database-sync.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { DatabaseSyncService } from './database-sync.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Sync')
 @Controller('sync')
 export class DatabaseSyncController {
   constructor(private readonly databaseSyncService: DatabaseSyncService) {}
 
   @Post('genres')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Sync Genres', description: 'Synchronizes genres with the database.' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Genres synced successfully.' })
   async syncGenres(): Promise<{ message: string }> {
     await this.databaseSyncService.syncGenres();
     return { message: 'Genres synced successfully' };
@@ -14,6 +18,8 @@ export class DatabaseSyncController {
 
   @Post('movies')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Sync Movies', description: 'Synchronizes movies with the database.' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Movies synced successfully.' })
   async syncMovies(): Promise<{ message: string }> {
     await this.databaseSyncService.syncMovies();
     return { message: 'Movies synced successfully' };
